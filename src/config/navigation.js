691d88bddc744f4dd7346901190d70343d4268b7
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2022-02-21 08:43:45 -0800

    Update navigation.js

--- src/config/navigation.js
+++ src/config/navigation.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Signup,Login,Options,Application } from '../screens';
-
 const Stack = createNativeStackNavigator();
   function AppNavigation() {
     return (
